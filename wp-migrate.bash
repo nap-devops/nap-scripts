c2cb74d9cece869b895599b2e4aeb692630c19ac
seubpong.mon <pjame.fb@gmail.com>
2022-07-07 20:55:10 +0700

    Corrected typo

--- wp-migrate.bash
+++ wp-migrate.bash
@@ -9,7 +9,7 @@ PROFILE=$1
 TAR_NAME=wp-content.old.tar
 
 set -o allexport; source ".env-${PROFILE}.cfg"; set +o allexport
-set -o allexport; source "config/${PROFILE}.cfg"; set +o allexport
+set -o allexport; source "configs/${PROFILE}.cfg"; set +o allexport
 
 #Copy wp-content from original container
 sudo tar -cvf ${TAR_NAME} ${APP_PATH}/wp-content
