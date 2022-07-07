fdeab9821c00284ac23218ce160773801968f60a
seubpong.mon <pjame.fb@gmail.com>
2022-07-07 21:13:23 +0700

    Dump database

--- wp-migrate.bash
+++ wp-migrate.bash
@@ -8,11 +8,15 @@ fi
 PWD=$(pwd)
 PROFILE=$1
 TAR_NAME="${PWD}/wp-content.original.tar"
+DUMPED_FILE="${PWD}/${PROFILE}.dump.sql"
 
 set -o allexport; source ".env-${PROFILE}.cfg"; set +o allexport
 set -o allexport; source "configs/${PROFILE}.cfg"; set +o allexport
 
-#Copy wp-content from original container
+#Tar wp-content from original container
 cd ${APP_PATH}
 sudo tar -cvf ${TAR_NAME} wp-content
 cd ${PWD}
+
+sudo docker exec -it wp-${DB_CONTAINER_NAME} sudo docker exec -it ${DB_CONTAINER_NAME} \
+    mysqldump -u root --password=${DB_PASSWORD} wordpress > ${DUMPED_FILE}
